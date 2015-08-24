702e32f14a8590e6f6adbf99af153bafd4b8d992
chocobo555 <ian.harris1@T3600-NICKEL.iam.local>
2015-08-24 14:01:59 -0500

    Worked on SpellActivation script and prefabbed current spells.

--- WolfWoman/Assets/Scripts/CraftSpell.cs
+++ WolfWoman/Assets/Scripts/CraftSpell.cs
@@ -1,23 +1,69 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
+using System.Collections.Generic;
 
 public class CraftSpell : MonoBehaviour 
 {
 
+	public ParseText myParseText;
+	public List<Button> allButtons;
+
+	List<string> myObtainedKeywords;
+	List<Button> spellsToActivate;
+	Rect myRect;
+	bool callActivate = false;
+
 
 	// Use this for initialization
-	void Start () 
+	void Start() 
 	{
-	
+
+		myObtainedKeywords = myParseText.ObtainedKeyWords;
+		myRect = gameObject.GetComponent<RectTransform>().rect;
+
+	}
+
+
+	void ActivateSpell()
+	{
+		foreach(string keyWord in myObtainedKeywords) 
+		{
+			if (keyWord == "Veridian") 
+			{
+				//spellsToActivate.Add(
+			}
+		}
+
+
+
+
+
+
+		for (int i = 0; i < spellsToActivate.Count; i++) 
+		{
+			Vector3 buttonPosition = new Vector3(myRect.width / 2, myRect.height / 2, 0);
+			Button mySpell = Instantiate(spellsToActivate[i], buttonPosition, Quaternion.Euler(0,0,0)) as Button;
+			mySpell.transform.SetParent(this.transform, false);
+		}
+
+	}
+
+
+	void LoadSpell()
+	{
+
 	}
 
 
 	// Update is called once per frame
-	void Update () 
+	void Update() 
 	{
-	
+		if(callActivate == false) 
+		{
+			ActivateSpell();
+			callActivate = true;
+		}
 	}
-
-
 
 }
